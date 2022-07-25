ddec392d5c0433c07222524b3f1ca2aa56c21bec
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-07-25 11:26:12 +0300

    completed

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <>
+    <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
